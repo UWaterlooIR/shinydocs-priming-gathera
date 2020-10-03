be3430e4a54aa45f45f59b304e6dc07e93034205
Mustafa Abualsaud <mabualsaud@outlook.com>
2020-10-03 18:59:54 -0400

    Allow GET in search

--- web/Web/web/search/urls.py
+++ web/Web/web/search/urls.py
@@ -5,20 +5,23 @@ from web.search import views
 app_name = "search"
 
 urlpatterns = [
-    path('', views.SearchHomePageView.as_view(),
+    path('', views.SearchView.as_view(),
          name='main'),
-    path('get_docs/', views.SearchListView.as_view(),
+    path('get_docs/',
+         views.SearchListView.as_view(),
          name='get_docs'),
-    path('get_single_doc/', views.SearchGetDocAJAXView.as_view(),
+    path('get_single_doc/',
+         views.SearchGetDocAJAXView.as_view(),
          name='get_doc'),
+    path('submit_search/',
+         views.SearchSubmitView.as_view(),
+         name='submit_search'),
+    path('query/<uuid:query_id>/',
+         views.SearchListView.as_view(),
+         name='query_result'),
 
     # Ajax views
-    path('post_search_status/', views.SearchInputStatusAJAXView.as_view(),
-         name='post_search_status'),
-    path('post_keystroke/', views.SearchKeystrokeAJAXView.as_view(),
-         name='post_keystroke'),
     path('post_search_request/', views.SearchButtonView.as_view(),
          name='post_search_request'),
-    path('post_search_doc_open/', views.SearchSearchDocOpenedView.as_view(),
-         name='post_search_doc_open'),
+
 ]
--- web/Web/web/search/views.py
+++ web/Web/web/search/views.py
@@ -1,18 +1,28 @@
+from config.utils import never_ever_cache
 import json
 import logging
 from collections import OrderedDict
 
 from braces import views
+from django.contrib import messages
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.template import loader
+from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views import generic
 import requests
 
+from web.core.mixin import RetrievalMethodPermissionMixin
 from web.interfaces.DocumentSnippetEngine import functions as DocEngine
 from config.settings.base import SEARCH_ENGINE
 from django.utils.module_loading import import_string
 from web.search import helpers
-from web.core.mixin import RetrievalMethodPermissionMixin
+from web.search.models import Query
+from web.search.models import SearchResult
+from web.search.models import SERPClick
 
 from web.interfaces.SearchEngine.base import SearchInterface
 
@@ -20,95 +30,126 @@ SearchEngine : SearchInterface = import_string(SEARCH_ENGINE)
 logger = logging.getLogger(__name__)
 
 
-class SearchHomePageView(views.LoginRequiredMixin,
-                         RetrievalMethodPermissionMixin,
-                         generic.TemplateView):
+class SearchView(views.LoginRequiredMixin,
+                 RetrievalMethodPermissionMixin,
+                 generic.TemplateView):
     template_name = 'search/search.html'
 
+    def get_context_data(self, **kwargs):
+        context = {
+            "isQueryPage": False,
+            "queryID": "NA",
+            "query": "",
+        }
+        return context
+
     def get(self, request, *args, **kwargs):
-        return super(SearchHomePageView, self).get(self, request, *args, **kwargs)
+        return super(SearchView, self).get(self, request, *args, **kwargs)
 
 
-class SearchInputStatusAJAXView(views.CsrfExemptMixin, views.LoginRequiredMixin,
-                                RetrievalMethodPermissionMixin,
-                                views.JsonRequestResponseMixin,
-                                generic.View):
-    require_json = False
+class SearchListView(views.LoginRequiredMixin, generic.TemplateView):
+    template_name = 'search/search.html'
 
-    def post(self, request, *args, **kwargs):
-        try:
-            client_time = self.request_json.get(u"client_time")
-            isFocused = self.request_json.get(u"isFocused")
-            page_title = self.request_json.get(u"page_title")
-            search_bar_value = self.request_json.get(u"search_bar_value")
-        except KeyError:
-            error_dict = {u"message": u"your input must include client_time, page_title"
-                                      u"search bar value, and isFocused."}
-            return self.render_bad_request_response(error_dict)
+    def get_context_data(self, **kwargs):
+        query_id = kwargs.get('query_id', None)
+        SERPInstance = get_object_or_404(SearchResult, query__query_id=query_id)
+        prev_clicks = SERPClick.objects.filter(username=self.request.user).values_list('docno', flat=True).distinct()
+        prev_clicks = list(prev_clicks)
 
-        context = {u"message": u"Your search input event has been recorded."}
-        return self.render_json_response(context)
+        context = {
+            "isQueryPage": True,
+            "queryID": query_id,
+            "query": SERPInstance.query.query,
+            "prevClickedUrlsDuringSession": prev_clicks,
+            "SERP": SERPInstance.SERP,
+        }
+        print(SERPInstance.SERP)
 
+        return context
 
-class SearchKeystrokeAJAXView(views.CsrfExemptMixin, views.LoginRequiredMixin,
-                              RetrievalMethodPermissionMixin,
-                              views.JsonRequestResponseMixin,
-                              generic.View):
-    require_json = False
-
-    def post(self, request, *args, **kwargs):
-        try:
-            client_time = self.request_json.get(u"client_time")
-            page_title = self.request_json.get(u"page_title")
-            character = self.request_json.get(u"character")
-            isSearchbarFocused = self.request_json.get(u"isSearchbarFocused")
-            search_bar_value = self.request_json.get(u"search_bar_value")
-        except KeyError:
-            error_dict = {u"message": u"your input must include client_time,"
-                                      u" page title, character, isSearchbarFocused,"
-                                      u" and search bar value."}
-            return self.render_bad_request_response(error_dict)
-
-        context = {u"message": u"Your keystroke has been recorded."}
-        return self.render_json_response(context)
+    @never_ever_cache
+    def get(self, request, *args, **kwargs):
+        return super().get(self, request, *args, **kwargs)
 
 
-class SearchListView(views.CsrfExemptMixin, generic.base.View):
-    template = 'search/search_list.html'
+class SearchSubmitView(views.CsrfExemptMixin,
+                       views.LoginRequiredMixin,
+                       generic.base.View):
+
+    def get_success_url(self, params):
+        return reverse('search:query_result', kwargs=params)
+
+    def get_failed_url(self,):
+        return reverse_lazy('failed')
 
     def post(self, request, *args, **kwargs):
-        template = loader.get_template(self.template)
-        try:
-            search_input = request.POST.get("search_input")
-            numdisplay = request.POST.get("numdisplay", 10)
-        except KeyError:
-            rendered_template = template.render({})
-            return HttpResponse(rendered_template, content_type='text/html')
-
         context = {}
 
         try:
-            results = SearchEngine.search(search_input, size=numdisplay)
-            documents = results["hits"]
-            document_ids = [hit["docno"] for hit in results["hits"]]
-            total_time = results["total_time"]
-            total_matches = results["total_matches"]
-        except (requests.exceptions.HTTPError, TimeoutError) as e:
-            context['error'] = f"{e}. Please check search server."
-            rendered_template = template.render(context)
-            return HttpResponse(rendered_template, content_type='text/html')
+            search_input = request.POST.get("search_input")
+        except KeyError:
+            context['error'] = "Error happened. No search input."
+            messages.add_message(request, messages.ERROR, context)
+            return HttpResponseRedirect(self.get_failed_url())
 
-        if total_matches:
-            documents = helpers.join_judgments(documents, document_ids,
-                                               self.request.user,
-                                               self.request.user.current_session)
+        # Create a query instance once we receive query
+        query_instance = Query.objects.create(
+            username=request.user,
+            query=search_input,
+            session=request.user.current_session,
+        )
 
-        context["documents"] = documents
-        context["query"] = search_input
-        context["total_time"] = "{0:.2f}".format(round(float(total_time), 2))
+        # Call search API to get search results
+        SERP = SearchEngine.search(search_input, size=10)
 
-        rendered_template = template.render(context)
-        return HttpResponse(rendered_template, content_type='text/html')
+        # Create search result instance with the result of the search API call.
+        SearchResult.objects.create(
+            username=request.user,
+            session=request.user.current_session,
+            query=query_instance,
+            SERP=SERP,
+        )
+
+        return JsonResponse({
+            "query_url": self.get_success_url({"query_id": str(query_instance.query_id)})
+        })
+
+
+# class SearchListView(views.CsrfExemptMixin, generic.base.View):
+#     template = 'search/search_list.html'
+#
+#     def post(self, request, *args, **kwargs):
+#         template = loader.get_template(self.template)
+#         try:
+#             search_input = request.POST.get("search_input")
+#             numdisplay = request.POST.get("numdisplay", 10)
+#         except KeyError:
+#             rendered_template = template.render({})
+#             return HttpResponse(rendered_template, content_type='text/html')
+#         context = {}
+#         documents_values, document_ids = None, None
+#         try:
+#             documents_values, document_ids, total_time = SearchEngine.get_documents(
+#                                                             search_input,
+#                                                             numdisplay=numdisplay
+#                                                          )
+#         except (TimeoutError, httplib2.HttpLib2Error):
+#             context['error'] = "Error happened. Please check search server."
+#
+#         if document_ids:
+#             # document_ids = helpers.padder(document_ids)
+#             documents_values = helpers.join_judgments(documents_values, document_ids,
+#                                                       self.request.user,
+#                                                       self.request.user.current_session)
+#
+#         context["documents"] = documents_values
+#         context["query"] = search_input
+#         if total_time:
+#             context["total_time"] = "{0:.2f}".format(round(float(total_time), 2))
+#
+#         rendered_template = template.render(context)
+#         return HttpResponse(rendered_template, content_type='text/html')
+
 
 
 class SearchButtonView(views.CsrfExemptMixin,
@@ -130,29 +171,6 @@ class SearchButtonView(views.CsrfExemptMixin,
             return self.render_bad_request_response(error_dict)
 
         context = {u"message": u"Your search request has been recorded."}
-        return self.render_json_response(context)
-
-
-class SearchSearchDocOpenedView(views.CsrfExemptMixin, views.LoginRequiredMixin,
-                                RetrievalMethodPermissionMixin,
-                                views.JsonRequestResponseMixin,
-                                generic.View):
-    require_json = False
-
-    def post(self, request, *args, **kwargs):
-        try:
-            client_time = self.request_json.get(u"client_time")
-            page_title = self.request_json.get(u"page_title")
-            query = self.request_json.get(u"query")
-            doc_id = self.request_json.get(u"doc_id")
-            doc_title = self.request_json.get(u"doc_title")
-            doc_search_snippet = self.request_json.get(u"doc_search_snippet")
-        except KeyError:
-            error_dict = {u"message": u"your input must include client_time,"
-                                      u" page title, query, and docid values"}
-            return self.render_bad_request_response(error_dict)
-
-        context = {u"message": u"Your document click request has been recorded."}
         return self.render_json_response(context)
 
 
