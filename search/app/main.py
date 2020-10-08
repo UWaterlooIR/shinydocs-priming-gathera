809e7acaedd62ed0036e09fc29e3dc81872623bc
Mustafa Abualsaud <mabualsaud@outlook.com>
2020-10-08 12:28:40 -0400

    Add minor changes to search endpoints

--- search/app/main.py
+++ search/app/main.py
@@ -1,16 +1,19 @@
 import os
-from fastapi import FastAPI, HTTPException
-from pyserini.search import SimpleSearcher
 from timeit import default_timer as timer
+
+from fastapi import FastAPI
+from fastapi import HTTPException
+from pyserini import index
+from pyserini.search import SimpleSearcher
 
 INDEX_PATH = os.environ['ANSERINI_INDEXI_PATH']
 
-# Initialize pyserini searcher
+# Initialize pyserini searcher and index reader
 searcher = SimpleSearcher(INDEX_PATH)
+index_reader = index.IndexReader(INDEX_PATH)
 
-# Configure BM25 parameters and use RM3 query expansion
+# Configure BM25 parameters
 searcher.set_bm25(0.9, 0.4)
-searcher.set_rm3(10, 10, 0.5)
 
 app = FastAPI()
 
@@ -26,37 +29,45 @@ def search(query: str, size: int = 100):
     hits = searcher.search(query, k=size)
     end = timer()
     total_time = end - start
+    hits_clean = []
+
+    for i in range(len(hits)):
+        content = index_reader.doc_contents(hits[i].docid)
+        h = {
+            "rank": i + 1,
+            "docno": hits[i].docid,
+            "score": hits[i].score,
+            "title": content[:55],
+            "snippet": content[:350]
+        }
+        hits_clean.append(h)
+
     return {
         "query": query,
         "total_matches": len(hits),
         "size": size,
         "total_time": total_time,
-        "hits":
-            [{
-                "rank": i + 1,
-                "docno": hits[i].docid,
-                "score": hits[i].score,
-                "title": searcher.doc(hits[i].docid).contents()[:15],
-                "snippet": searcher.doc(hits[i].docid).contents()[:50]
-            } for i in range(len(hits))]
+        "hits": hits_clean
     }
+
 
 @app.get("/docs/{docno}/content")
 def get_content(docno: str):
-    doc = searcher.doc(docno)
-    if doc is None:
+    content = index_reader.doc_contents(docno)
+    if content is None:
         raise HTTPException(status_code=404, detail="Doc not found")
     return {
         "docno": docno,
-        "content": doc.contents()
+        "content": content
     }
+
 
 @app.get("/docs/{docno}/raw")
 def get_content(docno: str):
-    doc = searcher.doc(docno)
-    if doc is None:
+    raw = index_reader.doc_raw(docno)
+    if raw is None:
         raise HTTPException(status_code=404, detail="Doc not found")
     return {
         "docno": docno,
-        "raw": doc.raw()
+        "raw": raw
     }
