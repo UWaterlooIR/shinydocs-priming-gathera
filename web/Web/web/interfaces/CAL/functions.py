bf51b02e9190073039af327b153df80418bb0c8d
Mustafa Abualsaud <mabualsaud@outlook.com>
2020-11-19 15:27:50 -0500

    Set timeout to 7 seconds

--- web/Web/web/interfaces/CAL/functions.py
+++ web/Web/web/interfaces/CAL/functions.py
@@ -20,7 +20,7 @@ def send_judgment(session, doc_id, rel, next_batch_size=5):
     :param next_batch_size:
     :return:
     """
-    h = httplib2.Http()
+    h = httplib2.Http(timeout=7)
     url = "http://{}:{}/CAL/judge"
 
     body = {'session_id': str(session),
