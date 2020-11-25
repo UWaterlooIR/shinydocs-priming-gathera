c4596d12fc6be8234d40424ca9f0cb5ea495de15
linhnhifm <43214352+linhnhifm@users.noreply.github.com>
2020-11-25 17:26:59 +0100

    Clickable link (#32)
    * Make link clickable

--- web/Web/web/static/js/document-viewer.js
+++ web/Web/web/static/js/document-viewer.js
@@ -434,6 +434,9 @@ docView.prototype = {
       }
 
     function updateMeta(content) {
+      if (isURL(content) === true){
+        content = content.link(content)
+      }
       const elm = $(options.documentMetaSelector);
       elm.html(content);
     }
@@ -1062,6 +1065,16 @@ docView.prototype = {
         return `Non${options.mainJudgingCriteriaName}`;
       }
       return "";
+    }
+
+    function isURL(str) {
+      var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+      return !!pattern.test(str);
     }
 
 
