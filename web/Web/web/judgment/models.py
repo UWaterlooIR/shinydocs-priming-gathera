df47878f2de4d803162d4bdd364c1a45e805a9dd
Abdul Manaam <amanaam123@gmail.com>
2024-10-21 17:29:36 +0000

    fixed session timer

--- web/Web/web/judgment/models.py
+++ web/Web/web/judgment/models.py
@@ -56,6 +56,27 @@ class Judgment(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def save(self, *args, **kwargs):
+        self.session.last_activity_timestamp = timezone.now()
+        self.session.save()
+        super(Judgment, self).save(*args, **kwargs)
+        if SessionTimer.objects.filter(session=self.session).exists():
+            session_timer = SessionTimer.objects.last()
+            if (timezone.now() - session_timer.end_time).seconds < 120:
+                session_timer.end_time = timezone.now()
+                session_timer.save()
+            else:
+                SessionTimer.objects.create(session=self.session,
+                                            start_time=timezone.now(),
+                                            end_time=timezone.now()
+                                            )
+        else:
+            SessionTimer.objects.create(session=self.session,
+                                       start_time=timezone.now(),
+                                       end_time=timezone.now()
+                                       )
+        print(str(self.session.last_activity_timestamp)+" updated")
 
     def __unicode__(self):
         return "{} on {}: {}".format(self.user, self.doc_id, self.relevance)
